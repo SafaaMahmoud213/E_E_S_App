23f59962081c3d5b34466d9ee39c6461dbe64fae
SafaaMahmoud213 <mahmoudsafaa463@gmail.com>
2025-07-23 18:26:34 +0300

    update

--- lib/core/api/dio_ consumer.dart
+++ lib/core/api/dio_ consumer.dart
@@ -1,13 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:my_app/core/api/api_consumer.dart';
 import 'package:my_app/core/api/api_interceptors.dart';
-import 'package:my_app/core/api/end_point.dart';
 import 'package:my_app/core/errors/server_exception.dart';
 
 class Dioconsumer extends ApiConsumer {
   final Dio dio;
   Dioconsumer({required this.dio}) {
-    dio.options.baseUrl =EndPoint.baseUrl;
+    dio.options.baseUrl = "EndPoint.baseUrl";
     dio.interceptors.add(ApiInterceptors());
     dio.interceptors.add(
       LogInterceptor(
@@ -42,9 +41,17 @@ class Dioconsumer extends ApiConsumer {
   }
 
   @override
-  Future<dynamic> get(String path, {Object? data, Map<String, dynamic>? queryParameter}) async {
+  Future<dynamic> get(
+    String path, {
+    Object? data,
+    Map<String, dynamic>? queryParameter,
+  }) async {
     try {
-      final response = await dio.get(path, data: data, queryParameters: queryParameter);
+      final response = await dio.get(
+        path,
+        data: data,
+        queryParameters: queryParameter,
+      );
       return response.data;
     } on DioException catch (e) {
       handelDioExceptio(e);
--- lib/core/api/end_point.dart
+++ lib/core/api/end_point.dart
@@ -1,11 +1,10 @@
 class EndPoint {
-  static String baseUrl = "pppp";
   static String signin = "sign in";
-
 }
-class ApiKey{
+
+class ApiKey {
   static String email = "email";
-   static String pass = "pass";
-    static String token = "token";
-     static String id = "id";
+  static String pass = "pass";
+  static String token = "token";
+  static String id = "id";
 }
